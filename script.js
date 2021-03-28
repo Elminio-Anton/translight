e184c34d404cd97467bd3fdc2945cb8f8f1ba255
Anton <elminio@ukr.net>
2021-03-29 00:33:30 +0300

    first final version 1.0

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+	let portfolioSlidesContainer = document.querySelector(".slides-container")
+	let slidesCoords = [0, 570, 1140]
+	let currentImageSlide = 0
+
+	document.querySelector(".image-slider-left").addEventListener("click", (event) => {
+		event.preventDefault()
+
+		console.log("left")
+		if (currentImageSlide === 0) {
+			scrollH(portfolioSlidesContainer, slidesCoords[slidesCoords.length - 1], 200)
+			currentImageSlide = slidesCoords.length - 1
+		} else {
+			scrollH(portfolioSlidesContainer, slidesCoords[--currentImageSlide])
+		}
+	})
+
+	document.querySelector(".image-slider-right").addEventListener("click", (event) => {
+		event.preventDefault()
+		console.log("right")
+
+		if (currentImageSlide === slidesCoords.length - 1) {
+			scrollH(portfolioSlidesContainer, slidesCoords[0], 200)
+			currentImageSlide = 0
+		} else {
+			scrollH(portfolioSlidesContainer, slidesCoords[++currentImageSlide])
+		}
+	})
+})
+
+const scrollH = (node, leftPosition, steps = 50) => {
+	let path = leftPosition - node.scrollLeft
+	let step = path / steps
+
+	let i = 0
+	let intervalId = setInterval(() => {
+		i++
+		if (i === steps) {
+			node.scrollLeft = leftPosition
+			clearInterval(intervalId)
+		}
+		node.scrollLeft += step
+	}, 5)
+}
